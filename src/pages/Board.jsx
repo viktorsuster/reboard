20e5e8ef1b2f124f376486c46add1159fb4cb4d3
Sebastian Andil <sebastian.andil@gmail.com>
2021-05-26 11:15:39 +0200

    Add create taskGroups functionality
    Co-authored-by: Viktor Šuster <viktorsuster@users.noreply.github.com>

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,30 +1,56 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
-import { Box, Center, SimpleGrid } from '@chakra-ui/react'
+import { Box, Button, Center, Input, SimpleGrid } from '@chakra-ui/react'
 import { TaskGroup } from '../components/TaskGroup'
 import { Task } from '../components/Task'
-import { getBoard } from '../utils/api'
+import { getBoard, createTaskGroup } from '../utils/api'
 
 const Board = () => {
   const { id } = useParams()
   const [board, setBoard] = React.useState({})
+  const [taskGroupName, setTaskGroupName] = React.useState('')
+
+  const fetchData = React.useCallback(async () => {
+    const data = await getBoard(id)
+    setBoard(data)
+  }, [id])
 
   React.useEffect(() => {
     try {
-      const fetchData = async () => {
-        const data = await getBoard(id)
-        setBoard(data)
-      }
       fetchData()
     } catch (e) {
       // do nothing
     }
-  }, [id])
+  }, [fetchData])
 
   return (
     <Box h="100vh">
       <Center>
         <Box>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault()
+              await createTaskGroup(Number(id), taskGroupName)
+              setTaskGroupName('')
+              fetchData()
+            }}
+          >
+            <Input
+              colorScheme="facebook"
+              variant="outline"
+              width="50vw"
+              ml="3"
+              mr="3"
+              placeholder="Name of the new task group..."
+              value={taskGroupName}
+              onChange={(e) => {
+                setTaskGroupName(e.target.value)
+              }}
+            />
+            <Button mb="1" colorScheme="facebook" type="submit">
+              Add task group
+            </Button>
+          </form>
           <Center>
             <SimpleGrid mt="10" columns="2" spacingX="20px" spacingY="20px">
               {

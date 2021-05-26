141b07af0436ff05b33b7e1f3070ed0b46d812c8
Sebastian Andil <sebastian.andil@gmail.com>
2021-05-26 11:52:37 +0200

    Polish Task Group
    Co-authored-by: Viktor Šuster <viktorsuster@users.noreply.github.com>

--- src/components/TaskGroup.jsx
+++ src/components/TaskGroup.jsx
@@ -1,24 +1,27 @@
 import * as React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text, VStack } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
 // import { Link } from 'react-router-dom'
 
 export const TaskGroup = ({ title, color, children }) => {
   return (
     <Box p="3" w="250px" h="max-content" borderRadius="md" shadow="base">
-      <Box borderRadius="md" shadow="base">
+      <Box borderRadius="md">
         <Text
           p="7"
           backgroundColor={color}
-          borderTopRadius="md"
+          borderRadius="md"
           textAlign="center"
           color="white"
           fontSize="2xl"
           fontWeight="bold"
+          mb="2"
         >
           {title}
         </Text>
-        {children}
+        <VStack alignItems="stretch" spacing="2">
+          {children}
+        </VStack>
         {/* <Link to={linkTo}>More</Link> */}
       </Box>
     </Box>
